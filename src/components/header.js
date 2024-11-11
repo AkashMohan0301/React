d22bbe95ed797fc1c8f160fe60d974d891e583ef
AkashMohan0301 <akashmohan940@gmail.com>
2024-11-11 19:30:30 +0530

    Functions

--- src/components/header.js
+++ src/components/header.js
@@ -5,6 +5,8 @@ const Header = () => {
     const getTheYear = () =>{
         const newdate = new Date();
         return newdate.getFullYear();
+        // return newdate.toLocaleDateString();
+
     }
 
 
@@ -13,7 +15,9 @@ const Header = () => {
         <header>
             <h1>My React Apps</h1>
             <div>
-                The Date is {getTheYear()}    
+                The Date is {getTheYear()}     
+                <p>Sum = {5+5}</p>
+                
             </div>
         </header>
     )
