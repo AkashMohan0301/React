aa43171bbcf9d2a2e91f26ab197c6864023c0096
AkashMohan0301 <akashmohan940@gmail.com>
2024-11-11 19:54:19 +0530

    same file styling

--- src/components/header.js
+++ src/components/header.js
@@ -3,8 +3,23 @@ import React,{Component} from "react";
 
 
     // Class based Components  in class we works with properties an methods
+    
+    // let styles = {
+    //     h1: {
+    //     background : "#03a9fa",
+    //     color: "#000000",
+    //     padding: "10px",
+    //     fontFamily:'Anton',
+    //     textAlign: 'center'
+        
+    //     },
+    //     input: {
+    //         width: "100%"
+    //     }
+    // }
 
     class Header extends Component {
+
 
         render(){
             return (
@@ -12,7 +27,7 @@ import React,{Component} from "react";
                     <React.Fragment>
 
                           
-                    <h1>Welcome</h1>
+                    <h1 style={styles.h1}>Welcome</h1>
                     <div>
                         Input : 
 
